ac88e07b5b27100196dc132439d6a96c3b8255ce
Itzmatt86 <87198253+Itzmatt86@users.noreply.github.com>
2026-02-24 15:19:42 +0000

    Platformering

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,23 +27,40 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    //createPlatform(xPos, yPos, Width, Height, "Color")
 
-
+    createPlatform(100-2, 700-2, 4, 4, "black");
+    createPlatform(200-2, 600-2, 4, 4, "black");
+    createPlatform(300-2, 500-2, 4, 4, "black");
+    createPlatform(400-2, 400-2, 4, 4, "black");
 
 
     // TODO 3 - Create Collectables
+    //createCollectable("name", xPos, yPos, gravity, bounce)
 
 
+createCollectable("database", 180, 100, 0.1, 1);
+createCollectable("database", 280, 100, 0.1, 1);
+createCollectable("database", 380, 100, 0.1, 1);
+createCollectable("database", 480, 100, 0.1, 1);
+createCollectable("database", 580, 100, 0.1, 1);    
 
-    
     // TODO 4 - Create Cannons
+//createCannon("top / right / bottom / left", position, Speed);
+createCannon("top", 259, 3000);
+createCannon("top", 359, 1000);
+createCannon("top", 459, 1000);
+createCannon("top", 559, 1000);
+createCannon("top", 659, 1000);
 
-
-    
+    //random timer on cannonballs
+    //random stats on collectables
+    //Change hallebot / find sprite sheet
+    //moving platforms
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
